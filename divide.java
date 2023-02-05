85676c292c143e70bfa2bf2c3010e752e0ff611a
govind kulkarni <govindkulkarni201996@gmail.com>
2023-02-05 13:33:23 +0530

    second commit

---
+++ divide.java
@@ -0,0 +1,13 @@
+class math{
+
+    public static void main(String[] args){
+
+        int a, b, res;
+
+        a=50; b= 10;
+
+        res =a/b;
+
+        system.out.print(res);
+    }
+}
--- sums.c
+++ sums.c
@@ -6,7 +6,9 @@ int main()
     a=5, b=10;
 
     res = a + b;
+    res = a - b;
 
+    
     printf("sum of two no is = ", res);
 
     return 0;
